1cbb4e62ca868e417c3130697ab95eedb8946866
mweb10 <michanyweb@gmail.com>
2025-05-16 09:28:26 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {render} from 'react-dom'
+
+import MultiDropdownApp from './ordered-examples/04-multi-select'
+
+function App() {
+  return <MultiDropdownApp />
+}
+
+render(<App />, document.getElementById('root'))
---
+++ src/ordered-examples/01-basic-autocomplete.js
@@ -0,0 +1,56 @@
+// here's an extremely bare bones example of an autocomplete
+import React from 'react'
+import Downshift from 'downshift'
+
+const items = [
+  {value: 'apple'},
+  {value: 'pear'},
+  {value: 'orange'},
+  {value: 'grape'},
+  {value: 'banana'},
+]
+
+export default () => (
+  <Downshift
+    onChange={selection => alert(`You selected ${selection.value}`)}
+    itemToString={item => (item ? item.value : '')}
+  >
+    {({
+      getInputProps,
+      getItemProps,
+      getLabelProps,
+      getMenuProps,
+      isOpen,
+      inputValue,
+      highlightedIndex,
+      selectedItem,
+    }) => (
+      <div>
+        <label {...getLabelProps()}>Enter a fruit</label>
+        <input {...getInputProps()} />
+        <ul {...getMenuProps()}>
+          {isOpen
+            ? items
+                .filter(item => !inputValue || item.value.includes(inputValue))
+                .map((item, index) => (
+                  <li
+                    {...getItemProps({
+                      key: item.value,
+                      index,
+                      item,
+                      style: {
+                        backgroundColor:
+                          highlightedIndex === index ? 'lightgray' : null,
+                        fontWeight: selectedItem === item ? 'bold' : 'normal',
+                      },
+                    })}
+                  >
+                    {item.value}
+                  </li>
+                ))
+            : null}
+        </ul>
+      </div>
+    )}
+  </Downshift>
+)
